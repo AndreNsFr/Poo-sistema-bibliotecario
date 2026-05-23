8c556b033c08da9c6dd6eca9ae0b9907e53a99dd
André do Nascimento <andredonascimentoferreira309@gmail.com>
2026-05-22 22:34:03 -0300

    Iniciando a desenvolver classe responsável pelas entradas

---
+++ src/main/java/classes/Entrada.java
@@ -0,0 +1,18 @@
+package classes;
+
+import java.util.Scanner;
+
+public class Entrada {
+
+    public void entradaCriarLivro(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do livro:");
+
+
+
+
+        scanner.close();
+    }
+
+}
