f1d17e5e2f80a36021d629ed337369e44227d4b5
André do Nascimento <andredonascimentoferreira309@gmail.com>
2026-05-23 22:30:40 -0300

    Testes a cerca da entrada do usuário

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,35 @@ import classes.Biblioteca;
 import classes.Livro;
 import classes.Usuario;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Biblioteca biblioteca = new Biblioteca();
+        Scanner scanner = new Scanner(System.in);
+
+        // se pode utilizar a mesma instancia de scanner para input, não sabia.
+        System.out.println("Olá! Digite nome de livro");
+        String nome_livro = scanner.nextLine();
+        System.out.println("Agora o autor dele");
+        String autor_livro = scanner.nextLine();
+        System.out.println("Digite o ISBN:");
+        String isbn_livro = scanner.nextLine();
+        System.out.println("Por ultimo, a quantidade disponivel");
+        int qtd_livro = scanner.nextInt();
+        scanner.close();
+
         Livro livro_criado = biblioteca.adicionarLivro(
-                "O programador Pragmático",
-                "David Thomas & Andrew Hunt" ,
-                "9788582606872",
-                10
+                nome_livro,
+                autor_livro,
+                isbn_livro,
+                qtd_livro
         );
-        Usuario usuario_criado = biblioteca.cadastrarUsuario("André");
-        biblioteca.realizarEmprestimo(usuario_criado, livro_criado);
+
+        livro_criado.exibirInformacoes();
 
 
-        Livro busca = biblioteca.buscarLivro("programador");
-        if(busca != null){
-            busca.exibirInformacoes();
-        }else{
-            System.out.println("Livro não encontrado :(");
-        }
+
 
     }
 }
